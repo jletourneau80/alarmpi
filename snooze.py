81a91087e4f06475f16682af605a2b721e369a93
Jason Letourneau <jletourneau80@gmail.com>
2015-11-24 15:03:06 -0500

    making snooze work

--- snooze.py
+++ snooze.py
@@ -6,7 +6,8 @@ iter = users_cron.find_command('sudo python /home/pi/alarmpi/sound_the_alarm.py'
 #disable the alarm
 for job in iter:
 	if job.is_enabled():
-		job.enable(False)
+		subprocess.call("sudo kill $(ps aux | grep sound_the_alarm.py | awk '{ print $2 }')", shell=True)
+		subprocess.call("sudo kill $(ps aux | grep mpg | awk '{ print $2 }')", shell=True)
 	else:
 		job.enable(True)
 		subprocess.call('sudo python sound_the_alarm.py', shell=True)
